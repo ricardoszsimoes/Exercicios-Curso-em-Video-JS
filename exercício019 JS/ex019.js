b2095808452577845f2a98aa07003d14d488e903
Ricardo Simões <147839494+ricardoszsimoes@users.noreply.github.com>
2023-11-10 19:52:41 -0300

    Update 019 JS
    - Finalizado e corrigido a atividade 019 de JavaScript.

--- exercício019 JS/ex019.js
+++ exercício019 JS/ex019.js
@@ -1,13 +1,21 @@
 function clicou() {
     var num1 = Number(prompt('Primeiro valor:'));
     var num2 = Number(prompt('Segundo valor:'));
-    var ari = prompt(`Valores informados: ${num1} e ${num2}.<br>O que vamos fazer?<[1] Somar<br>[2] Subtrair<br>[3] Multiplicar<br> [4] Dividir`)
+    var ari = prompt(`Valores informados: ${num1} e ${num2}.\nO que vamos fazer?\n[1] Somar\n[2] Subtrair\n[3] Multiplicar\n[4] Dividir`)
     
-    if (ari = 1) {
+    res.innerHTML = `<h2>Calculando...</h2>`
+
+    if (ari == 1) {
         var conta = num1 + num2;
-        res.innerHTML = `<p>${conta}</p>`
-    } else if (ari = 2) {
-        var conta  = num1 - num2;
-        res.innerHTML = `<p>${conta}</p>`
+        res.innerHTML += `<p>${num1} + ${num2} = ${conta}</p>`
+    } else if (ari == 2) {
+        var conta = num1 - num2;
+        res.innerHTML += `<p>${num1} - ${num2} = ${conta}</p>`
+    } else if (ari == 3) {
+        var conta = num1 * num2;
+        res.innerHTML += `<p>${num1} x ${num2} = ${conta}</p>`
+    } else if (ari == 4) {
+        var conta = num1 / num2;
+        res.innerHTML += `<p>${num1} / ${num2} = ${conta}</p>`
     }
 }
